41ae2b6d01f0c9a3aa6f68664b3e67e45ff70d8a
Ismael Soares <78043399+maelsilvatt@users.noreply.github.com>
2024-04-14 15:39:26 -0300

    Added custom model section
    Trained model should come in "model = YOLO('your_model.pt')".

--- yolo_inference.py
+++ yolo_inference.py
@@ -1,10 +1,13 @@
 from ultralytics import YOLO
 import torch
 
-model = YOLO('yolov8x')
+# using our trained model
+model = YOLO('models/yolov5_last.pt')
 
-# Check for CUDA device and set it
-device = 'cuda' if torch.cuda.is_available() else 'cpu'
-print(f'Using device: {device}')
+# setting inference device as CUDA (if available)
+device = 'cuda' if torch.cuda.is_available else 'cpu'
 
-model.predict('input_videos\input_video.mp4')  
+# store and predict
+conf = 0.2 # this confidence value gave us the best results
+
+result = model.predict('input_videos/input_video.mp4', device=device, save=True, conf=conf)
