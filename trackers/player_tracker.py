d59f30bd0d9d8ac877330ee4b02b1f3ff01eb367
Ismael Soares <78043399+maelsilvatt@users.noreply.github.com>
2024-04-20 18:33:24 -0300

    Added bbox drawing methods

--- trackers/player_tracker.py
+++ trackers/player_tracker.py
@@ -1,4 +1,5 @@
 from ultralytics import YOLO
+import cv2
 
 class PlayerTracker:
     def __init__(self, model_path):
@@ -13,6 +14,7 @@ class PlayerTracker:
         
         return player_detections
 
+    # Given a frame, detects players and returns their position
     def detect_frame(self, frame):
         results = self.model.track(frame, persist=True)[0]
         id_name_dict = results.names
@@ -28,3 +30,27 @@ class PlayerTracker:
                 player_dict[track_id] = result
 
         return player_dict
+    
+    # Given a frame, returns bounding boxes with texts on detected players, if there is any
+    def draw_bboxes(self, video_frames, player_detections):
+        output_video_frames = []
+
+        for frame, player_dict in zip(video_frames, player_detections):
+            # Draw bounding boxes
+            for track_id, bbox in player_dict.items():
+                x1, y1, x2, y2 = bbox
+
+                # Draw a text alongside bounding boxes
+                text = f'Player ID: {track_id}'
+                org = (int(bbox[0]), int(bbox[1] - 100))
+
+                cv2.putText(img=frame, text=text, org=org, fontFace=cv2.FONT_HERSHEY_COMPLEX, fontScale=0.9, color=(0, 0, 255), thickness=2)
+
+                rec = (int(x1), int(y1), int(x2), int(y2))
+
+                frame = cv2.rectangle(img=frame, rec=rec, color=(0, 0, 255), thickness=2)
+            
+            # Draw new data into actual frame
+            output_video_frames.append(frame)
+
+        return output_video_frames
