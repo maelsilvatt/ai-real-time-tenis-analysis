b0fc50a023f1c115403204b9ded79d4bbe496d00
Ismael Soares <ismaelsoares.pro@gmail.com>
2024-04-25 21:34:08 -0300

    Draw court line keypoints and ball tracker fixes

--- main.py
+++ main.py
@@ -8,6 +8,11 @@ def main():
     input_video_path = "input_videos/input_video.mp4"
     video_frames =  read_video(input_video_path)
 
+    # Detect court lines
+    court_model_path = "models/keypoints_model.pth"
+    court_line_detector = CourtLineDetector(court_model_path)
+    court_keypoints = court_line_detector.predict(video_frames[0])
+
     # Detect players
     player_tracker = PlayerTracker(model_path='models/yolov8x.pt')
     player_detections = player_tracker.detect_frames(video_frames, read_from_stub=True, stub_path="tracker_stubs/player_detections.pkl")
@@ -18,7 +23,8 @@ def main():
 
     # Draw bounding boxes
     output_video_frames = player_tracker.draw_bboxes(video_frames, player_detections)
-    output_video_frames = player_tracker.draw_bboxes(video_frames, ball_detections)
+    output_video_frames = ball_tracker.draw_bboxes(video_frames, ball_detections)
+    output_video_frames = court_line_detector.draw_keypoints(video_frames, court_keypoints)
 
     # Save output video
     save_video(video_frames, "output_videos/output_video.avi")
