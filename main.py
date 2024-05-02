6f8678c6813bb89dfaa4dbf3ddb99e0fc91c4dd5
Ismael Soares <ismaelsoares.pro@gmail.com>
2024-05-01 21:58:37 -0300

    Updated player only tracking

--- main.py
+++ main.py
@@ -9,15 +9,15 @@ def main():
     video_frames =  read_video("input_videos/input_video.mp4")
 
     # Detect court lines
-    keypoints_detector = KeyPointsDetector(model_path="models/keypoints_model.pth")
+    keypoints_detector = KeyPointsDetector("models/keypoints_model.pth")
     court_keypoints = keypoints_detector.predict(video_frames[0])
 
     # Detect players
-    player_tracker = PlayerTracker(model_path='models/yolov8x.pt')
-    player_detections = player_tracker.detect_frames(video_frames, read_from_stub=True, stub_path="tracker_stubs/player_detections.pkl")
+    player_tracker = PlayerTracker('models/yolov8x.pt')
+    player_detections = player_tracker.detect_frames(video_frames, court_keypoints, read_from_stub=True, stub_path="tracker_stubs/player_detections.pkl")
     
     # Detect ball
-    ball_tracker = BallTracker(model_path='models/yolov5_last.pt')
+    ball_tracker = BallTracker('models/yolov5_last.pt')
     ball_detections = ball_tracker.detect_frames(video_frames, read_from_stub=True, stub_path="tracker_stubs/ball_detections.pkl")
 
     # Draw bounding boxes
