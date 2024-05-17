23a6865c7ea5372c11ebbea2c223b104e56ba31f
maelsilvatt <ismaelsoares.pro@gmail.com>
2024-05-16 21:30:40 -0300

    Minor changes on video saver function

--- utils/video_utils.py
+++ utils/video_utils.py
@@ -17,8 +17,10 @@ def read_video(video_path):
 
 # Saves a list of frames as a video
 def save_video(output_video_frames, output_video_path):
-    fourcc = cv2.VideoWriter_fourcc(*"MJPG")
-    out = cv2.VideoWriter(output_video_path, fourcc, 24, (output_video_frames[0].shape[1], output_video_frames[0].shape[0]))
+    fourcc = cv2.VideoWriter_fourcc(*'MJPG')
+    width = output_video_frames[0].shape[1]
+    heigth = output_video_frames[0].shape[0]
+    out = cv2.VideoWriter(output_video_path, fourcc, 24, (width, heigth))
 
     for frame in output_video_frames:
         out.write(frame)
